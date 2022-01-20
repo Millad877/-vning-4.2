e888a41a30b2b5d052efac0e263d0d5be96346ff
Millad877 <millad.akrawi@elev.ga.ntig.se>
2022-01-20 11:17:35 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Övning_4._2
+{
+    static class Program
+    {
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
